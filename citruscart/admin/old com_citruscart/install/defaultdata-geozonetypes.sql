8720a2be731a7196a7096b62e693ac97de82928b
weblogicx <89janpriya@gmail.com>
2014-05-12 13:40:28 +0530

    --revomed old folder

--- citruscart/admin/old com_citruscart/install/defaultdata-geozonetypes.sql
+++
@@ -1,6 +0,0 @@
--- -----------------------------------------------------
--- Dumping data for table `#__citruscart_geozonetypes`
--- -----------------------------------------------------
-INSERT IGNORE INTO `#__citruscart_geozonetypes` (`geozonetype_id`, `geozonetype_name`, `created_date`, `modified_date`) VALUES
-(1, 'Tax Zones', NOW(), NOW()),
-(2, 'Shipping Zones', NOW(), NOW());
--- citruscart/admin/old com_citruscart/views/elementuser/tmpl/default.php
+++
@@ -1,2 +0,0 @@
-<?php defined('_JEXEC') or die('Restricted access'); ?>
-<?php include JPATH_SITE . '/libraries/dioscouri/component/view/element/default.php'; ?>
