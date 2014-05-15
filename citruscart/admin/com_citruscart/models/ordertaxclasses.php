d9cd8ae6c6643f0784d8ed8e9e3e8cb91058f22f
Kumar Ramalingam <weblogicx@weblogicx3>
2014-05-15 12:14:25 +0530

    -- Update files in admin

--- citruscart/admin/com_citruscart/models/ordertaxclasses.php
+++ citruscart/admin/com_citruscart/models/ordertaxclasses.php
@@ -1,11 +1,13 @@
 <?php
-/**
- * @package	Citruscart
- * @author 	Dioscouri Design
- * @link 	http://www.dioscouri.com
- * @copyright Copyright (C) 2009 Dioscouri Design. All rights reserved.
- * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
-*/
+/*------------------------------------------------------------------------
+# com_citruscart
+# ------------------------------------------------------------------------
+# author   Citruscart Team  - Citruscart http://www.citruscart.com
+# copyright Copyright (C) 2014 Citruscart.com All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://citruscart.com
+# Technical Support:  Forum - http://citruscart.com/forum/index.html
+-------------------------------------------------------------------------*/
 
 /** ensure this file is being included by a parent file */
 defined('_JEXEC') or die('Restricted access');
