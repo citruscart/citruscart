79ae600c3f45199839f5e8003484a356224173e5
JoomlaCorner <akarawuth@joomlacorner.com>
2014-10-08 11:10:09 +0700

    fixed file conflict

--- citruscart/script.citruscart.php
+++ citruscart/script.citruscart.php
@@ -34,27 +34,9 @@ class Com_CitruscartInstallerScript{
 		$prefix = $db->getPrefix();
 
 		//address
-<<<<<<< HEAD
 
 		if(!in_array($prefix.'citruscart_wishlistitems', $tables)){
-
-			$query = "CREATE  TABLE IF NOT EXISTS `#__citruscart_wishlistitems` (
-      `wishlistitem_id` int(11) NOT NULL AUTO_INCREMENT,
-      `wishlist_id` int(11) NOT NULL,
-					`user_id` int(11) NOT NULL,
-					`wishlist_name` varchar(255) NOT NULL,
-					`privacy` int(11) NOT NULL DEFAULT '1' COMMENT 'public = 1, linkonly = 2, private  = 3',
-      `session_id` varchar(255) NOT NULL,
-      `product_id` int(11) NOT NULL,
-      `vendor_id` int(11) NOT NULL,
-      `product_attributes` text NOT NULL COMMENT 'A CSV of productattributeoption_id values, always in numerical order',
-      `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      `wishlistitem_params` text COMMENT 'Params for the wishlist item',
-      PRIMARY KEY (`wishlistitem_id`)) ENGINE=InnoDB  DEFAULT CHARSET=utf8;";
-=======
-
-		if(!in_array($prefix.'citruscart_wishlistitems', $tables)){
-
+     
 			$query = "CREATE TABLE IF NOT EXISTS `#__citruscart_wishlistitems` (
   `wishlistitem_id` int(11) NOT NULL AUTO_INCREMENT,
   `wishlist_id` int(11) NOT NULL,
@@ -70,7 +52,6 @@ class Com_CitruscartInstallerScript{
   PRIMARY KEY (`wishlistitem_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 			";
->>>>>>> currency
 			$this->_executeQuery($query);
 		}
 
